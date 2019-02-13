cdc637608d6528b510da1a4cdc79764f648dec9b
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-02-13 06:20:04 -0700

    Added link to router BrowserRouter coponent

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -12,17 +12,21 @@ const AddExpensePage = () => <div>Add expence Component</div>;
 
 const EditExpensePage = () => <div>Edit expence Component</div>;
 const HelpPage = () => <div>Help Page</div>;
-const NotFoundPage = () => <div>404xxxx</div>;
+const NotFoundPage = () => (
+  <div>
+    404 - <Link to="/">Go home</Link>
+  </div>
+);
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
       <Route component={NotFoundPage} />
-    </div>
+    </Switch>
   </BrowserRouter>
 );
 
