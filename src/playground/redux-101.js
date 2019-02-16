e3afb7f3dbe42e9c7a150bdebb4cce5894869818
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-02-16 05:27:12 -0700

    How to dispatch actions in redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,27 +3,37 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { count: state.count + 1 };
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
+      return { count: state.count + incrementBy };
     case "DECREMENT":
-      return { count: state.count - 1 };
-      case "RESET":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
+      return { count: state.count - decrementBy };
+    case "RESET":
       return { count: 0 };
+    case "SET":
+      return { count: action.count };
     default:
       return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
 //  Actions - Object that gets send to the store.
 //  I'd like to increment the count.
 store.dispatch({
-  type: "INCREMENT"
+  type: "INCREMENT",
+  incrementBy: 5
 });
-console.log(store.getState());
+//unsubscribe();
 store.dispatch({
   type: "INCREMENT"
 });
-console.log(store.getState());
+
 //RESET - set the count equal to zero.
 store.dispatch({
   type: "RESET"
@@ -32,5 +42,13 @@ store.dispatch({
 store.dispatch({
   type: "DECREMENT"
 });
-console.log(store.getState());
-//  I'd like to reset the count to zero.
+
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: "SET",
+  count: 101
+});
