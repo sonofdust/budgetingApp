8471939c7634fcaee9439bed1f152baf0e0a818f
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-02-16 12:45:31 -0700

    Combining reducers

--- src/playground/redux-budget.js
+++ src/playground/redux-budget.js
@@ -1,6 +1,42 @@
 import { createStore, combineReducers } from "redux";
 
-console.log("Doing the Budget");
+//  ADD_EXPENSE
+//  REMOVE_EXPENSE
+//  EDIT_EXPENSE
+//  SET_TEXT_FILTER
+//  SORT_BY_DATE
+//  SORT_BY_AMOUNT
+//  SET_START_DATE
+//  SET_END_DATE
+
+//Expenses Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Filter reducer.
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//Store Creation
+const store = createStore(
+  combineReducers({ expenses: expensesReducer, filters: filtersReducer })
+);
+console.log(store.getState());
 
 const demoState = {
   expense: [
